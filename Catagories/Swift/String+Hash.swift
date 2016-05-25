0ad183f5838cdd52c9153b0e07ff1b03d3e72b1c
Detailscool <496389006@qq.com>
2016-05-25 23:28:49 +0800

    add String+Hash

---
+++ Catagories/Swift/String+Hash.swift
@@ -0,0 +1,86 @@
+//
+//  String+Hash.swift
+//
+//  Created by Detailscool on 16/5/25.
+//  Copyright © 2016年 Detailscool. All rights reserved.
+//
+
+import Foundation
+
+// #import <CommonCrypto/CommonHMAC.h> in BridgingHeader
+
+enum CryptoAlgorithm {
+    
+    case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+    
+    var HMACAlgorithm: CCHmacAlgorithm {
+        var result: Int = 0
+        switch self {
+        case .MD5:      result = kCCHmacAlgMD5
+        case .SHA1:     result = kCCHmacAlgSHA1
+        case .SHA224:   result = kCCHmacAlgSHA224
+        case .SHA256:   result = kCCHmacAlgSHA256
+        case .SHA384:   result = kCCHmacAlgSHA384
+        case .SHA512:   result = kCCHmacAlgSHA512
+        }
+        return CCHmacAlgorithm(result)
+    }
+    
+    var digestLength: Int {
+        var result: Int32 = 0
+        switch self {
+        case .MD5:      result = CC_MD5_DIGEST_LENGTH
+        case .SHA1:     result = CC_SHA1_DIGEST_LENGTH
+        case .SHA224:   result = CC_SHA224_DIGEST_LENGTH
+        case .SHA256:   result = CC_SHA256_DIGEST_LENGTH
+        case .SHA384:   result = CC_SHA384_DIGEST_LENGTH
+        case .SHA512:   result = CC_SHA512_DIGEST_LENGTH
+        }
+        return Int(result)
+    }
+}
+
+extension String {
+    
+    func hmac(algorithm: CryptoAlgorithm, key: String) -> String {
+        let str = self.cStringUsingEncoding(NSUTF8StringEncoding)
+        let strLen = Int(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+        let digestLen = algorithm.digestLength
+        let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLen)
+        let keyStr = key.cStringUsingEncoding(NSUTF8StringEncoding)
+        let keyLen = Int(key.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+        
+        CCHmac(algorithm.HMACAlgorithm, keyStr!, keyLen, str!, strLen, result)
+        
+        let digest = stringFromResult(result, length: digestLen)
+        
+        result.dealloc(digestLen)
+        
+        return digest
+    }
+    
+    private func stringFromResult(result: UnsafeMutablePointer<CUnsignedChar>, length: Int) -> String {
+        let hash = NSMutableString()
+        for i in 0..<length {
+            hash.appendFormat("%02x", result[i])
+        }
+        return String(hash)
+    }
+    
+    var md5 : String{
+        let str = self.cStringUsingEncoding(NSUTF8StringEncoding)
+        let strLen = CC_LONG(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
+        let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLen);
+        
+        CC_MD5(str!, strLen, result);
+        
+        let hash = NSMutableString();
+        for i in 0 ..< digestLen {
+            hash.appendFormat("%02x", result[i]);
+        }
+        result.destroy();
+        
+        return String(format: hash as String)
+    }
+}
